6cc14e3dc5a8091bd28227c87cf4341d80afc174
nick <nick_php@163.com>
2018-06-08 14:31:51 +0800

    add favicon

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
     <title>@yield('title', 'l.hellocode.name') - l.hellocode.name - Laravel</title>
     <link rel="stylesheet" href="/css/app.css" />
+    <link rel="shortcut icon" href="/favicon.png">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="Cache-Control" content="no-transform" />
+    <meta http-equiv="Cache-Control" content="no-siteapp" />
+    <meta name="author" content="Nick">
+    <meta name="Keywords" content="php,laravel,vue,js,mysql,css,html5" />
+    <meta name="Description" content="php,laravel,vue,js,mysql,css,html5信息。" />
 </head>
 <body>
     @include('layouts._header')
