54aaddc8d12fb5b96e8cfc37378aeeab731b7e2d
nick <nick_php@163.com>
2018-06-08 15:31:52 +0800

    add

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,13 +1,23 @@
-<header class="navbar navbar-fixed-top navbar-inverse">
+
+<header class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            <a href="{{ route('home') }}" id="logo">Sample App</a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{ route('help') }}">帮助</a></li>
-                    <li><a href="#">登录</a></li>
-                </ul>
-            </nav>
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand hidden-sm" href="{{ route('home') }}" id="logo">Sample App</a>
+        </div>
+        <div class="navbar-collapse collapse" role="navigation">
+            <ul class="nav navbar-nav">
+                <li><a href="#">登录</a></li>
+                <li><a href="{{ route('signup') }}">注册</a></li>
+                <li><a href="{{ route('help') }}">帮助</a></li>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
         </div>
     </div>
 </header>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -22,5 +22,7 @@
             @include('layouts._footer')
         </div>
     </div>
+    <script src="/js/app.js"></script>
+
 </body>
 </html>
