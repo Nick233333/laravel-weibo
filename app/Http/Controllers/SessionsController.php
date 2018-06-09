5772fa6c19356438013415fe96cdce1b65595a0c
nick <nick_php@163.com>
2018-06-09 13:44:55 +0800

    update

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,9 +25,7 @@ class SessionsController extends Controller
             return redirect()->route('users.show', [Auth::user()]);
         } else {
             session()->flash('danger', '邮箱或密码不匹配');
-            session()->flash('email', $request->email);
-            session()->flash('password', $request->password);
-            return redirect()->back();
+            return redirect('/login')->withInput();
         }
     }
 
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -15,12 +15,12 @@
 
                     <div class="form-group">
                         <label for="email">邮箱：</label>
-                        <input type="text" name="email" class="form-control" value="{{ session()->get('email') }}">
+                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
                     </div>
 
                     <div class="form-group">
                         <label for="password">密码：</label>
-                        <input type="password" name="password" class="form-control" value="{{ session()->get('password') }}">
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
                     </div>
                     <div class="checkbox">
                         <label><input type="checkbox" name="remember"> 记住我</label>
