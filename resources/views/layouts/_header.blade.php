732184af03f05b622a7d48467c6146306876c1b1
nick <nick_php@163.com>
2018-06-10 14:17:44 +0800

    update

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -37,9 +37,9 @@
                         </li>
                     @else
                         <li><a href="{{ route('login') }}">登录</a></li>
+                    @endif
                         <li><a href="{{ route('about') }}">关于</a></li>
                         <li><a href="{{ route('help') }}">帮助</a></li>
-                    @endif
                 </ul>
 
                 >
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,7 +7,7 @@
                 <section class="status_form">
                     @include('shared._status_form')
                 </section>
-                <h3>微博列表</h3>
+                <h3>动态列表</h3>
                 @include('shared._feed')
             </div>
             <aside class="col-md-4">
@@ -19,14 +19,9 @@
     @else
         <div class="jumbotron">
             <h1>Hello Laravel</h1>
-            <p class="lead">
-                你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
-            </p>
+            <p class="lead">Hello Code</p>
             <p>
-                一切，将从这里开始。
-            </p>
-            <p>
-                <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+                <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册</a>
             </p>
         </div>
     @endif
